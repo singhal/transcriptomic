91ed7aaa4f49ec193a3d381cb37498c385fe2466
singhal <sonal.singhal1@gmail.com>
2013-01-25 14:42:21 -0800

    first commit

--- 3scrubReads.pl
+++ 3scrubReads.pl
@@ -2,114 +2,147 @@
 # a script to clean up reads (adaptor/duplicate/contamination removal), trimming #
 # external dependencies: blat (v34), flash, trimmomatic                          #
 # written by Sonal Singhal, sonal.singhal1 [at] gmail.com, 7 Dec 2011            #
+# updated by Tyler Linderoth, tylerp.linderoth@gmail.com, 10 Dec 2012             # 								                 
 ##################################################################################
 
 use warnings;
 use strict;
+use Getopt::Std;
 
-#home directory with all my files; make sure to end with a backslash
-my $dir = '/media/DataDrive/sutureGenomics/';
-my $tmpdir = '/media/DataDrive/tmp/';
-#this file has all the info about my library, and creates a directory of all my files
-my $lib = '/home/singhal/sutureGenomics/library';
-#this file has a list of the adaptor sequences for my library, including the reverse complements.
-my $adaptorFile = '/home/singhal/sutureGenomics/illuminaAdapters.fa';
-#path to trimmomatic executable
-my $trimmomatic = '/home/singhal/programs/trimmomatic/trimmomatic-0.17.jar';
-#path to blat executable
-my $blat = 'blat';
-#indexed contaminant genome; here this includes ecoli and rRNA
-my $contam = '/home/singhal/sutureGenomics/contaminants.fa';
-my $readLength = 100;
-my $nper = 0.6; #will get rid of reads for which more than $nper of bases are NNs
-my $aper = 0.5; #will get rid of reads with any runs of bases longer than $aper*$readLength
-#path to flash executable
-my $flash = '/home/singhal/programs/flash/flash';
+my $version = '0.1.3'; # as of Dec. 6 2012
 
-#defines the library
-open(IN, "<$lib");
-my %lib;
-while (<IN>) {
-	chomp(my $line = $_);
-	my @d = split(/\t/, $line);
-	my $lib = $d[2] . '/' . $d[0] . '/';
-	$lib{$d[0]} = {'dir' => $lib, 'index' => $d[1]};	
-	}
-close(IN);
+die (qq/
+#########################
+# 3scrubReads v$version #
+#########################
+Usage: 3scrubReads.pl options
 
-#defines the adaptors
-open(AD, "<$adaptorFile");
-my %ad;
-while(<AD>) {
-	chomp(my $line = $_);
-	if ($line =~ m/>(\S+)/) {
-		my $id = $1;
-		chomp (my $seq = <AD>);
-		$ad{$id} = $seq;
-		}
-	}
-close(AD);	
+Options:
+-d	CHAR	directory containing fastq files 
+-o	CHAR	output directory 
+-t	CHAR	temporary directory
+-a	FILE	file containing adaptor sequences for my library including reverse compliments
+-g	CHAR	path to trimmoatic executable
+-b	CHAR	path to blat executable
+-c	CHAR	contaminant file (ex. indexed contaminant genome including with ecoli and rRNA)
+-f	CHAR	path to flash executable
+-l	INT	read length [100]
+-n	FLOAT	will get rid of reads for which more than INT*read_length bases are NNs [0.6]
+-r	FLOAT	will get rid of reads with any runs of bases longer than FLOAT*read_length [0.5]
+-h	Y|N	perform hard trimming if -h is set to Y. No hard trimming if -h is set to N [Y]
+-w	Y|N	warn if entire sequences are removed by hard trimming if -w is set to Y [N]    
+-1	INT	number of base pairs to hard trim from 5' end of read 1 [0]
+-2	INT	number of base pairs to hard trim from 3' end of read 1 [0]
+-3	INT	number of base pairs to hard trim from 5' end of read 2 [0]
+-4	INT	number of base pairs to hard trim from 3' end of read 2 [0]
+
+Option Examples:
+-d '\/Users\/sarahhykin\/Desktop\/Anolis_FF_data\/Sample_JMSH001B_index7\/Sample_1B_catted\/'
+-o '\/Users\/sarahhykin\/Desktop\/Anolis_FF_data\/Sample_JMSH001B_index7\/Sample_1B_catted\/scrubbed\/'
+-t '\/Users\/sarahhykin\/Desktop\/Anolis_FF_data\/Sample_JMSH001B_index7\/Sample_1B_catted\/tmp\/'
+-a '\/Users\/sarahhykin\/Desktop\/Anolis_FF_data\/Sample_JMSH001B_index7\/Sample_1B_catted\/adaptorseqs_index7.fa'
+-g '\/Users\/sarahhykin\/bin\/Trimmomatic-0.20\/trimmomatic-0.20.jar'
+-b '\/Users\/sarahhykin\/bin\/blatSrc\/blat\/blat'
+-c '\/Users\/sarahhykin\/Desktop\/Anolis_FF_data\/bin\/contaminant.fa'
+-f '\/Users\/sarahhykin\/bin\/FLASH\/flash'
+
+Notes:
+-Hard trimming only works on files with illumina Casava 1.8+ header format:
+ example: \@HS2:241:C0W6TACXX:4:1101:1130:2215 1:N:0:ACAGTG
+-Hard trimming is performed AFTER trimmomatic run
+-Assumes a library naming convention of ONLY letters & numbers with a _run[1|2].txt ending
+-Home directory with all my files; make sure to end with a backslash
+-Assumes the FLASH version released on 14 May 2012
+\n/) if !@ARGV;
+
+my %opts = (
+d => undef,
+o => undef,
+t => undef,
+a => undef,
+g => undef,
+b => undef,
+c => undef,
+f => undef,
+l => 100,
+n => 0.6,
+r => 0.5,
+h => 'Y',
+w => 'N',
+1 => 0,
+2 => 0,
+3 => 0,
+4 => 0
+);
+
+getopts('d:o:t:a:g:b:c:f:l:n:r:h:w:1:2:3:4:', \%opts);
+
+my $dir = $opts{d};
+my $outdir = $opts{o};
+my $tmpdir = $opts{t};
+my $adaptorFile = $opts{a};
+my $trimmomatic = $opts{g};
+my $blat = $opts{b};
+my $flash = $opts{f};
+my $contam = $opts{c};
+my $readLength = $opts{l};
+my $nper = $opts{n};
+my $aper = $opts{r};
 
 ###########################
 # run the subroutines     #
-###########################	
+# edit at your risk       #
+###########################
 
-foreach my $lib (sort {$a cmp $b} keys %lib) {
-	my $subdir = $dir . $lib{$lib}{'dir'};
-	if (-d $subdir) {
-		unless (-f $subdir . $lib . '_1p_final.fastq.gz') {
-			print "Starting to process files from $lib! So exciting.\n";
-			my $file1gz = $subdir . $lib . '_1.fastq.gz';
-			my $file2gz = $subdir . $lib . '_2.fastq.gz';
-			my $file1   = $subdir . $lib . '_1.fastq';
-			my $file2   = $subdir . $lib . '_2.fastq';
+mkdir($outdir) unless (-d $outdir);
+my @files = <$dir*run1*fastq>;
+	
+foreach my $file1 (@files) {
+	my $file2 = $file1;
+	$file2 =~ s/run1/run2/;
+	my $lib = $1 if $file1 =~ m/([A-Z|0-9]+)_run1/i;
 
-			my $start1 = time;
-			unzip($file1gz,$file2gz);
-			my $time1 = int((time - $start1)/60);
-			print "Unzipped files in $lib in $time1 minutes! Now we are cooking.\n";
+	my $start2 = time;	
+	my $dup = $file1 . '.duplicates.out';
+	duplicates($file1, $file2, $dup);
+	my $time2 = int((time - $start2)/60);
+	print "Found duplicates in $lib in $time2 minutes! Now this is something...\n";
 
-			my $start2 = time;	
-			my $dup = $subdir . 'duplicates.out';
-			duplicates($file1, $file2, $dup);
-			my $time2 = int((time - $start2)/60);
-			print "Found duplicates in $lib in $time2 minutes! Now this is something...\n";
+	my $start3 = time;
+	my $low = $file1 . '.lowComplexity.out';
+	removeLowComplexity($file1,$low); removeLowComplexity($file2,$low);
+	my $time3 = int((time - $start3)/60);
+	print "Found low complexity reads in $lib in $time3 minutes! Whew, almost there!\n";
 
-			my $start3 = time;
-			my $low = $subdir . 'lowComplexity.out';
-			removeLowComplexity($file1,$low); removeLowComplexity($file2,$low);
-			my $time3 = int((time - $start3)/60);
-			print "Found low complexity reads in $lib in $time3 minutes! Whew, almost there!\n";
+	my $start4 = time;
+	my ($out1p,$out2p,$out1u,$out2u) = adaptorRemovalandTrim($outdir,$file1,$file2,$adaptorFile);
+	my $time4 = int((time - $start4)/60);
+	print "Found adaptor and trimmed in $lib in $time4 minutes! Slowly but surely...\n";
 
-			my $start4 = time;
-			my ($out1p,$out2p,$out1u,$out2u) = adaptorRemovalandTrim($subdir,$file1,$file2,\%ad);
-			my $time4 = int((time - $start4)/60);
-			print "Found adaptor and trimmed in $lib in $time4 minutes! Slowly but surely...\n";
+	if ($opts{h} =~ /Y/i) {
+	my $start4b = time;
+	($out1p, $out2p, $out1u, $out2u) = hardTrim($out1p, $out2p, $out1u, $out2u, $opts{1}, $opts{2}, $opts{3}, $opts{4});
+	my $time4b = int((time-$start4b)/60);
+	print "Hard trimmed in $lib in $time4b\n";
+	}
+	
+	my $start5 = time;
+	my $contaminants =  $file1 . '.contam.out';
+	removeContamination($out1p,$out2p,$out1u,$out2u,$contaminants,$blat);
+	my $time5 = int((time - $start5)/60);
+	print "Removed contamination in $lib in $time5 minutes! It's going...\n";
 
-			my $start5 = time;
-			my $contaminants = $subdir . 'contam.out';
-			removeContamination($out1p,$out2p,$out1u,$out2u,$contaminants,$blat);
-			my $time5 = int((time - $start5)/60);
-			print "Removed contamination in $lib in $time5 minutes! It's going...\n";
-
-			my $start6 = time;
-			my $unpaired = $subdir . $lib . '_u_trimmed.fastq';
-			my $flashout = runFlash($out1p,$out2p,$flash);
-			processFlash($flashout,$out1p,$out2p,$out1u,$out2u,$unpaired);
-			my $time6 = int((time - $start6)/60);
-			print "Combined reads in $lib in $time6 minutes. Next to last step!\n";
+	my $start6 = time;
+	my $unpaired = $outdir . $lib . '_u_trimmed.fastq';
+	my $flashout = runFlash($out1p,$out2p,$flash);
+	processFlash($flashout,$out1p,$out2p,$out1u,$out2u,$unpaired);
+	my $time6 = int((time - $start6)/60);
+	print "Combined reads in $lib in $time6 minutes. Next to last step!\n";
 		
-			my $start7 = time;
-			my $final = makeFinalFiles($subdir,$out1p,$out2p,$unpaired,$dup,$low,$contaminants);
-			my $time7 = int((time - $start7)/60);
-			print "Made final libraries in $lib in $time7 minutes! Getting there!\n";
-
-			my $start8 = time;
-			eraseAndZip($out1p,$out2p,$unpaired,$final,$file1,$file2);
-			my $time8 = int((time - $start8)/60);
-			print "Zipped and cleaned up in $lib in $time8 minutes! Done!\n";
-			}
-		}
+	my $start7 = time;
+	my $final = makeFinalFiles($outdir,$out1p,$out2p,$unpaired,$dup,$low,$contaminants);
+	my $time7 = int((time - $start7)/60);
+	print "Made final libraries in $lib in $time7 minutes! Getting there!\n";
 	}
 
 
@@ -122,19 +155,24 @@ sub processFlash {
 	my @flashout = @{$files};	
 
 	#first need to rename my not combined files
-	my $call1 = system("mv $flashout[1] $file1");
-	my $call2 = system("mv $flashout[2] $file2");
+	my $call1 = system("mv $flashout[1] $file1") if (-f $flashout[1]);
+	my $call2 = system("mv $flashout[2] $file2") if (-f $flashout[2]);
 
 	#now need to combine all my single reads
+	if (-f $flashout[0]) {	
+		my $call3 = system("cat $file3 $file4 $flashout[0] > $unpaired");
+		}
+	else {
+		my $call3 = system("cat $file3 $file4 > $unpaired");
+		}
 	
-	my $call3 = system("cat $file3 $file4 $flashout[0] > $unpaired");
 	unlink($file3); unlink($file4); unlink($flashout[0]);
 	}
 
 sub runFlash {
 	my ($file1,$file2,$flash) = @_;
-	my $call = system("$flash $file1 $file2 -x 0.02");
-	my @newfiles = ('extendedFrags.fastq', 'notCombined_1.fastq', 'notCombined_2.fastq');
+	my $call = system("$flash $file1 $file2 -x 0.35 -r 159 -f 200");
+	my @newfiles = ('out.extendedFrags.fastq', 'out.notCombined_1.fastq', 'out.notCombined_2.fastq');
 	return(\@newfiles);	
 	}
 
@@ -158,7 +196,7 @@ sub makeFinalFiles {
 		open(IN, "<$read");
 		while(<IN>) {
 			my $line = $_;
-			if ($line =~ m/^\@(HS\S+)/) {
+			if ($line =~ m/^\@(H\S+:\S+)/) {
 				$id{$1}++;
 				}
 			}
@@ -167,14 +205,14 @@ sub makeFinalFiles {
 	my @reads = ($out1p, $out2p, $unpaired);
 	foreach my $read (@reads) {
 		my $id = $1 if $read =~ m/([A-Z|0-9|_]+)_trimmed/i;
-		my $paired = $1 if $read =~ m/([1|2])p_/;
+		my $paired = $1 if $read =~ m/([1|2])p/;
 		my %fr = ('1' => 'F', '2' => 'R');	
 		my $out = $subdir . $id . "_final.fastq";
 		push(@final,$out);
 		open(IN, "<$read"); open(OUT, ">$out");
 		while(<IN>) {
 			my $line = $_;
-			if ($line =~ m/^\@(HS\S+)/) {
+			if ($line =~ m/^\@(H\S+:\S+)/) {
 				my $readName = $1;
 				my $seq = <IN>;		
 				my $qualID = <IN>;
@@ -197,27 +235,6 @@ sub makeFinalFiles {
 	return(\@final);
 	}
 
-sub eraseAndZip {
-	my ($out1p,$out2p,$unpaired,$final,$file1,$file2) = @_;
-	
-	#need to erase all the trimmed files
-	unlink($out1p); unlink($out2p); unlink($unpaired);
- 
-	#need to compress my original files
-	my $call1 = system("gzip -1 $file1");
-	my $call2 = system("gzip -1 $file2");
-
-	#need to compress my final files
-	foreach my $file (@{$final}) {
-		my $call = system("gzip -1 $file");
-		}
-	}
-
-sub unzip {
-	my ($file1, $file2) = @_;
-	my $call1 = system("gunzip $file1");
-	my $call2 = system("gunzip $file2");
-	}
 
 sub duplicates {
 	my ($file1,$file2,$dup) = @_;	
@@ -280,7 +297,7 @@ sub removeDuplicates {
 
 	foreach my $tracker (keys %dup) {
 		foreach my $id (@{$dup{$tracker}}) {
-			$id = $1 if $id =~ m/^\@(\S+)/;
+			$id = $1 if $id =~ m/^\@(H\S+)/;
 			$dup_rev{$id} = $tracker;
 			}
 		}
@@ -329,11 +346,10 @@ sub removeLowComplexity {
 
 sub adaptorRemovalandTrim {
 	my ($subdir, $file1, $file2,$ad) = @_;
-	my %ad = %{$ad};
 
 	my ($out1p,$out2p,$out1u,$out2u);	
 	
-	if ($file1 =~ m/([A-Z|0-9]+)_[1|2]\./i) {	
+	if ($file1 =~ m/([A-Z|0-9]+)_run1/i) {	
 		#need to define the file names
 		my $lib = $1;
 		$out1p = $subdir . $lib . '_1p_trimmed.fastq';
@@ -341,20 +357,82 @@ sub adaptorRemovalandTrim {
 		$out1u = $subdir . $lib . '_1u_trimmed.fastq';
 		$out2u = $subdir . $lib . '_2u_trimmed.fastq';
 
-		#need to print a file with the Illumina adaptors in it	
-		my $ad = $dir . 'adaptors.fa';
-		open(OUT, ">$ad");
-		my @ad = ('adapt' . $lib{$lib}{'index'}, 'universal', 'adapt' . $lib{$lib}{'index'} . '_rc');
-		foreach (@ad) {
-			print OUT ">", $_, "\n", $ad{$_}, "\n";
-			}
-		close(OUT);
-
 		my $call = system("java -classpath $trimmomatic org.usadellab.trimmomatic.TrimmomaticPE -phred33 $file1 $file2 $out1p $out1u $out2p $out2u ILLUMINACLIP:$ad:2:40:12 LEADING:5 TRAILING:5 SLIDINGWINDOW:4:20 MINLEN:36");
 		}
-	unlink($ad);
 	return($out1p,$out2p,$out1u,$out2u);
 	}
+	
+sub hardTrim {
+	my @fastqfiles = @_[0 .. 3]; #array w/ fastq files
+	my ($f5, $f3, $r5, $r3) = @_[4 .. 7]; # number of bases to trim	
+	my @newfiles;
+	foreach (@fastqfiles) {
+	# make sure file exists and has size != 0
+	die ("$_ does not exist!\n") if (!-e $_);
+	die ("$_ has size zero!\n") if (-z $_);
+	my ($filename, $oldfile) = ($_, $_);
+	# open files for reading/writing
+		open(FASTQ, '<', $_);	
+		$filename =~ s/trimmed\.fastq$/trimmedH\.fastq/; # new fastq file name
+		push @newfiles, $filename; # store new file names in this array	
+		open(OUT, '>', $filename);
+		# check whether file contains read 1 or read 2 and set trimming parameters
+		my $head;
+		do {$head = <FASTQ> } until ($head =~ /^\@\S+:\d+:/);
+		my $direction = $1 if ($head =~ /\s+([1|2]):[A-Z]:\d+:[A-Z]+\s*$/i);
+		my $cut5 = ($direction == 1) ? $f5 : $r5;
+		my $cut3 = ($direction == 1) ? $f3 : $r3;
+		# process input fastq file	
+		seek (FASTQ, 0, 0); # rewind fastq file before processing it
+		while (<FASTQ>) {
+			# ensure that you start at the header		
+			if ($_ =~ /^\@\S+:\d+:/) {			
+				$head = $_;
+				# trim and print sequence
+				my $seq;			
+				do { $seq = <FASTQ> } until ($seq =~ /^[A-Z]+/i);
+				# trim 5' end
+				if ($seq =~ /^[A-Z]{$cut5}([A-Z]+)/i) {
+					$seq = $1; 
+				} else {
+					# toss out read warning
+					if ($opts{w} =~ /Y/i) {
+						chomp $head;
+						print STDERR "$head removed by hard trimming!\n";
+					}
+					next;
+				}
+				# trim 3' end 
+				if ($seq =~ /^([A-Z]+)[A-Z]{$cut3}\s*$/i) {
+					$seq = $1;
+				} else {
+					if ($opts{w} =~ /Y/i) {
+						chomp $head;
+						print STDERR "$head removed by hard trimming!\n";
+					}		
+					next;
+				}   
+				print OUT "$head$seq\n"; # print sequence
+				# print optional sequence identifier
+				my $id2;
+				do { $id2 = <FASTQ> } until ($id2 =~ /^\+/);
+				print OUT $id2;
+				# trim and print quality scores
+				my $qual;
+				do { $qual = <FASTQ> } until ($qual =~ /^\S+/);
+				$qual = $1 if $qual =~ /^\S{$cut5}(\S+)/; # trim 5' end
+				$qual = $1 if $qual =~ /^(\S+)\S{$cut3}\s*$/;; # trim 3' end 	
+				print OUT "$qual\n";		
+			} else {
+				next;
+			} 
+		}
+		close FASTQ;
+		close OUT; 
+		unlink $oldfile; # delete previous version of fastq files
+	}
+	return @newfiles;
+}
 
 sub removeContamination {
 	my ($out1p,$out2p,$out1u,$out2u,$contaminants,$blat) = @_;
@@ -367,7 +445,7 @@ sub removeContamination {
 		open(OUT, ">$fasta");
 		while(<IN>) {
 			chomp(my $line = $_);
-    		if ($line =~ m/^\@(HS\S+)/) {
+    		if ($line =~ m/^\@(H\S+)/) {
 				my $id = $1;
 				my $seq = <IN>;
 				print OUT ">", "$1", "\n", $seq;
